c1b7ee7aa019daad83b0a97000fcd61f46c9d34f
Matheus <you@example.com>
2022-07-26 11:54:40 -0300

    v3

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import pandas as pd
 from PyQt5 import uic, QtWidgets
-from PyQt5.QtWidgets import QLineEdit
-
+from PyQt5.QtWidgets import QLineEdit,QComboBox
 textocaminho = ''
-bancodedados = ''
-user = ''
-passw = ''
-host = ''
-nomedabd = ''
-tabela = ''
-
 
 def caminhoExcel():
     textocaminho = QtWidgets.QFileDialog.getOpenFileName()[0]
@@ -19,23 +11,48 @@ def caminhoExcel():
 
 def executar():
     bancodedados = janela.nome_banco_de_dados.currentText()
-    user = janela.janela.nome_user.currentText()
-    passw = janela.nome_senha.currentText()
-    host = janela.nome_host.currentText()
-    nomedabd = janela.nome_db.currentText()
-    tabela = janela.nome_tabela.currentText()
+    user = janela.nome_user.text()
+    passw = janela.nome_senha.text()
+    host = janela.nome_host.text()
+    nomedabd = janela.nome_db.text()
+    tabela = janela.nome_tabela.text()
     df = pd.read_excel(textocaminho)
     from sqlalchemy import create_engine
     # format: mysql://user:pass@host/db
-    # engine = create_engine(bd'://'user':'senha'@'host'/'database)
-    # df.to_sql(tabela, con=engine)
-    print(bancodedados + '://' + user + ':' + passw + '@' + host + '/' + nomedabd + tabela)
+    engine = create_engine(bancodedados'://'user':'passw'@'host'/'nomedabd)
+    df.to_sql(tabela, con=engine)
+
 
 
 app = QtWidgets.QApplication([])
 janela = uic.loadUi("tela_excel_para_banco_de_dados.ui")
 janela.pushButton.clicked.connect(caminhoExcel)
-janela.nome_banco_de_dados.addItems(["postgresql", "mysql", "mssql", "sqlite", "oracle" ])
 janela.pushButton_2.clicked.connect(executar)
+
+janela.nome_banco_de_dados = QComboBox(janela)
+janela.nome_banco_de_dados.move(130,130)
+janela.nome_banco_de_dados.resize(351,31)
+janela.nome_banco_de_dados.addItems(["postgresql", "mysql", "mssql", "sqlite", "oracle" ])
+
+janela.nome_db = QLineEdit(janela)
+janela.nome_db.move(130,170)
+janela.nome_db.resize(351,31)
+
+janela.nome_user = QLineEdit(janela)
+janela.nome_user.move(130,250)
+janela.nome_user.resize(351,31)
+
+janela.nome_senha = QLineEdit(janela)
+janela.nome_senha.move(130,290)
+janela.nome_senha.resize(351,31)
+
+janela.nome_host = QLineEdit(janela)
+janela.nome_host.move(130,210)
+janela.nome_host.resize(351,31)
+
+janela.nome_tabela = QLineEdit(janela)
+janela.nome_tabela.move(130,330)
+janela.nome_tabela.resize(351,31)
+
 janela.show()
 app.exec()
